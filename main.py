3109e7173feeda23052dc06adc41bcf07608b8de
djbios <dorandval@gmail.com>
2022-09-23 12:27:49 +0200

    Init

---
+++ main.py
@@ -0,0 +1,25 @@
+import yaml
+import jinja2
+
+def to_flat(d: dict, a=None, parent=''):
+    if a is None:
+        a = []
+
+    for k, v in d.items():
+        a.append((k, parent))
+        if v is not None:
+            assert isinstance(v, dict)
+            a = to_flat(v, a, k)
+
+    return a
+
+
+with open('./languages.yaml', 'r') as f:
+    data = yaml.safe_load(f)
+    res = to_flat(data)
+    with open('./index.html', 'r') as t:
+        env = jinja2.Environment()
+        template = env.from_string(t.read())
+        html = template.render(labels=[l for l, _ in res], parents=[p for _, p in res])
+        with open('./result.html', 'w') as o:
+            o.write(html)
